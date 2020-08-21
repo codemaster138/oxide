47d8139edfd50f301bb3b47ce932740d99a9e443
Jake <pygamer138@gmail.com>
2020-08-21 16:54:05 +0200

    _

--- runtime/index.js
+++ runtime/index.js
@@ -11,6 +11,7 @@ export const LexerOptions = {
         'PLUS': /\+/,
         'MINUS': /\-/,
         'POW': /\*\*/,
+        'NPOW': /\/\//,
         'MUL': /\*/,
         'DIV': /\//,
         'LPAREN': /\(/,
